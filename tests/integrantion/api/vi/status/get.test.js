ebcaea801df94cabcb613fd06f10e3fa9dcd9518
Leonardo Fagundes <89270130+leoFagundes@users.noreply.github.com>
2024-12-13 11:47:33 +0000

    add `api/v1/migrations` endpoint

--- tests/integrantion/api/vi/status/get.test.js
+++ tests/integrantion/api/vi/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.3");
+  expect(responseBody.dependencies.database.version).toEqual("16.6");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
