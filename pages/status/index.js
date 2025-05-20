a9736f47a20d3a9337915285509a5a3f751627b2
Leonardo Fagundes <89270130+leoFagundes@users.noreply.github.com>
2025-05-20 12:18:56 +0000

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,60 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function APIPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let UpdatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>última atualização: {UpdatedAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInfomation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInfomation = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInfomation}</div>
+    </>
+  );
+}
