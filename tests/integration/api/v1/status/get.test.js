a4c281971408876f915c727b17933c578a2c5902
Leonardo Fagundes <89270130+leoFagundes@users.noreply.github.com>
2025-05-22 17:11:53 +0000

    refactor: use `next-connect` in `/migrations` and `/status`

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,23 @@
+import orchestrator from "../orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody.dependencies.database.version).toEqual("16.6");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
+});
